651ee2a7f655b7801e426ca4ee88e9cfae200591
AbdelkaderBelhadj <abdelkader04belhadj@gmail.com>
2021-12-25 00:52:48 +0100

    Initialisation de projet

---
+++ src/main/java/com/example/gestionfacture/GestionfactureApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionfacture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionfactureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionfactureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestionfacture/GestionfactureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionfacture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionfactureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
